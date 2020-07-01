22300494aa9f624e42f58e3f95e1b5b1341842b8
shehab-ahamd <66440199+shehab-ahamd@users.noreply.github.com>
2020-07-01 18:01:03 +0300

    d
    *_
    *

---
+++ q/app/src/main/java/com/example/q/Movie.java
@@ -0,0 +1,9 @@
+package com.example.q;
+
+public class Movie {
+   private String title;
+    private String mainActor;
+    private Double movieRate;
+    private int pgRate;
+    private String qenre;
+}
